faddddb573ad7885c8d94c117f27831126171e35
Fer Guevara <kafera.g10@gmail.com>
2019-11-10 12:19:44 -0600

    Controladores
    Creación de los controladores de las pantallas del sistema

---
+++ AtenciónMédica/src/GUI/Controller/DetallesCitasController.java
@@ -0,0 +1,22 @@
+package GUI.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author JENNY
+ */
+public class DetallesCitasController implements Initializable {
+
+  /**
+   * Initializes the controller class.
+   */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // TODO
+  }  
+  
+}
