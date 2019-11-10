ee78e3b69ae281202dd00200f8e6cd93c7bc138e
Fer Guevara <kafera.g10@gmail.com>
2019-11-09 18:46:58 -0600

    Pantallas
    Creación de las pantallas del sistema

--- AtenciónMédica/src/Logica/Estudiante.java
+++ AtenciónMédica/src/Logica/Estudiante.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Logica;
 
 /**
- *
- * @author JENNY
+ * @
+ * @author Karla Fernanda Guevara Flores
  */
 public class Estudiante {
-    
+  private String nombre;
+  private String apellidoPaterno;
+  private String apellidoMaterno;
+  private String matricula;
+
 }
