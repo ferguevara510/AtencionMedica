0ad6546bd60030ea72231b58b37736a32f0ea10c
Fer Guevara <kafera.g10@gmail.com>
2019-11-12 12:12:37 -0600

    Pantallas-Proyecto
    Uní la creación de las pantallas con el proyecto

---
+++ AtenciónMédica/src/Controller/PrincipalController.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * Clase controlador de pantalla Principal
+ * 
+ * @author KARLA FERNANDA GUEVARA FLORES
+ * @version 1.0
+ * @since 09-11-2019
+ */
+public class PrincipalController {
+
+  @FXML
+  private Button estudiantes;
+  
+  @FXML
+  private Button productos;
+  
+  /**
+   * 
+   */
+}
